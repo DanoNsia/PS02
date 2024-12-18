3053ef82cef143402df7b8c98ab7de59bd1002a9
DanoNsia <sibraevd1998@gmail.com>
2024-12-18 14:58:11 +0300

    second
    ГОТОВАЯ/РАБОТА

--- third.py
+++ third.py
@@ -1,3 +1,17 @@
 import requests
-import pprint
 
+
+url = "https://jsonplaceholder.typicode.com/posts"
+
+data = {
+    "title" : "тестовый post запрос",
+    "body" : "тестовый контент post запрос",
+    "foo" : "ещё один тестовый запрос",
+    "userId" : 2
+}
+
+response = requests.post(url, data=data)
+
+print(response.status_code)
+
+print(f"ответ - {response.json()}")
